fc803b019c9d4162fe770ee68575b26a5f622614
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-06-29 21:07:06 -0600

    Add files via upload

---
+++ functional_Programming/main.py
@@ -0,0 +1,7 @@
+# Pure functions
+# def multiply_by2(li):
+#     new_list = [i * 2 for i in li]
+#     return new_list
+
+
+# print(multiply_by2([1, 2, 3]))
---
+++ functional_Programming/map.py
@@ -0,0 +1,18 @@
+# map, filter, zip, reduce functions
+# map
+# def multiply_by2(item):
+#     return item * 2
+
+# print(list(map(multiply_by2, [1, 2, 3])))
+
+# Filter
+my_list = [1, 2, 3]
+# def check_odd(item):
+#     return item % 2 == 1
+
+# print(list(filter(check_odd, my_list)))
+
+# Zip
+your_list = [10, 20, 30]
+thier_list = [5, 4, 3]
+print(list(zip(your_list, my_list, thier_list)))
